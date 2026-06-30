83ede045fc2efc20f502f6686ecd4999954cc6cc
Ibon Mugica <iboedu@hotmail.com>
2026-07-01 01:35:56 +0200

    Añadir test de compras

--- test_main.py
+++ test_main.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 from main import app
+from datetime import datetime, timezone
 
 client = TestClient(app)
 
@@ -17,7 +18,6 @@ def test_crear_activo():
     client.post("/registro", json={"username": "test_crear", "password": "test123"})
 
     response = client.post("/login", data={"username": "test_crear", "password": "test123"})
-    print(response.json())
     token = response.json()["access_token"]
 
     response = client.post("/activos", headers={"Authorization": f"Bearer {token}"}, json={"ticker": "MAIN", "nombre": "Main Street Capital"})
@@ -34,4 +34,15 @@ def test_listar_activos():
     
     # 3. Request con token
     response = client.get("/activos", headers={"Authorization": f"Bearer {token}"})
+    assert response.status_code == 200
+
+def test_crear_compra():
+    client.post("/registro", json={"username": "test_crear", "password": "test123"})
+
+    response = client.post("/login", data={"username": "test_crear", "password": "test123"})
+    token = response.json()["access_token"]
+
+    response = client.post("/activos", headers={"Authorization": f"Bearer {token}"}, json={"ticker": "MAIN", "nombre": "Main Street Capital"})
+
+    response = client.post("/compras", headers={"Authorization": f"Bearer {token}"}, json={"activo_id": 1, "fecha_compra": "2026-06-25", "precio": 55, "cantidad": 4, "tipo_cambio": 1.16})
     assert response.status_code == 200
